142b2af6ad374e49a2c88ae76a869aa66cd58f2d
EC2 Default User <ec2-user@ip-10-10-101-219.ap-northeast-2.compute.internal>
2024-06-24 10:56:22 +0000

    python

--- test-python/test_app.py
+++ test-python/test_app.py
@@ -9,4 +9,4 @@ def client():
 def test_hello(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.data == b'hello'
+    assert response.data == b'hello team5'
